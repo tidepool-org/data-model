aab5e851bddc894f3ed8819d21994f5c70e69b18
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-05-02 17:35:56 -0700

    add script for regenerating all doc templates

---
+++ regenerate-all-docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd bin/
+
+./docTemplateGenerator.js basal --subType scheduled
+./docTemplateGenerator.js basal --subType temp
+./docTemplateGenerator.js basal --subType suspend
+
+./docTemplateGenerator.js bolus --subType normal
+./docTemplateGenerator.js bolus --subType square
+./docTemplateGenerator.js bolus --subType dual/square
+
+./docTemplateGenerator.js bloodKetone
+
+./docTemplateGenerator.js cbg
+
+./docTemplateGenerator.js deviceEvent --subType alarm
+./docTemplateGenerator.js deviceEvent --subType calibration
+./docTemplateGenerator.js deviceEvent --subType prime
+./docTemplateGenerator.js deviceEvent --subType reservoirChange
+./docTemplateGenerator.js deviceEvent --subType status
+./docTemplateGenerator.js deviceEvent --subType timeChange
+
+./docTemplateGenerator.js smbg
+
+cd ../
